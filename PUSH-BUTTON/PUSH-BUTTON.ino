a2955ec2b54cccfbcfc1621009cc1f76220aafa9
khushipache1109-ux <khushipache1109@gmail.com>
2026-04-03 00:32:31 +0530

    Push-Button1LED

--- PUSH-BUTTON/PUSH-BUTTON.ino
+++ PUSH-BUTTON/PUSH-BUTTON.ino
@@ -1,9 +1,6 @@
 #define led 4
 #define button 8 
-#define led_t 2
-#define button_p 12 
- 
- 
+
 void setup() {
   Serial.begin(9600);
   pinMode(led,OUTPUT);
@@ -11,12 +8,9 @@ void setup() {
 }
 void loop() {
    bool x = digitalRead(button);
-   // this is for 1st button
-   bool y = digitalRead(button_p);
-   //this is for 2nd button
    Serial.print("Button status =");
    Serial.println(x);
-   Serial.println(y);
+ 
    if (x == 1){
     digitalWrite(led, HIGH);
     //Serial.print("LED"is turn ON);
@@ -25,11 +19,4 @@ void loop() {
       digitalWrite(led, LOW);
       //Serual.print("LED"is turn OFF);
     }
- if (y == 1){
-   digitalWrite(led_t, HIGH);
- }
-  else
-  {
-    digitalWrite(led_t, LOW);
-  }
   }
